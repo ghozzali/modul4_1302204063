559b65aef97784add9b37b763d19b17d5a4239a3
ghozzali <ghozal.perdana02@gmail.com>
2022-03-19 16:49:25 +0700

    menambahkan class Penjumlahan dengan methodJumlahTigaAngka

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JurnalMod4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // memanggil generic method
+            Console.WriteLine(Penjumlahan.JumlahTigaAngka<long>(1, 02, 20));
+
+
+        }
+    }
+
+    class Penjumlahan
+    {
+        public static T JumlahTigaAngka<T>(T input1, T input2, T input3)
+        {
+            dynamic a = input1;
+            dynamic b = input2;
+            dynamic c = input3;
+
+            return a + b + c;
+        }
+    }
+}
